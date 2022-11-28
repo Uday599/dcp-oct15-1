80710145bcf408babde30d652f3c19975ebefdb1
Uday <70462083+Uday599@users.noreply.github.com>
2022-11-28 19:29:13 +0530

    6

---
+++ bash_scripting/User input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter your skills:"
+read SKILL
+
+echo "Your $SKILL skill is in high Demand in the IT Industry."
+
+read -p 'Username: ' USR
+read -sp 'Password: ' pass # -s(suppress, like cant be seen) p-(prompt)
+
+echo
+
+echo "Login Successfull: Welcome USER $USR,"
